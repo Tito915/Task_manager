7b515d70dd3fa8be843750cdec73caeda4c3b752
Tito915 <titodossantos@icloud.com>
2024-10-29 17:21:13 -0300

    Ajuste do modulo Sales APP Caluladora

--- app.py
+++ app.py
@@ -110,7 +110,7 @@ def show_main_content():
 
         task_manager_options[selected_option]()
 
-    elif ambiente_normalizado == "salesapp":
+    elif ambiente_normalizado == "Sales app":
         sales_app_options = {
             "Visão Geral": 'visao_geral',
             "Metas de Vendas": 'metas_vendas',
