c5e9c5b216917c3fc0c659f4921feaaa94c178ae
Tito915 <titodossantos@icloud.com>
2024-10-30 16:18:45 -0300

    Correção dados do controle fiscal

--- sales_app/Dados_Controle_Ctrl_Fiscal.py
+++ sales_app/Dados_Controle_Ctrl_Fiscal.py
@@ -1,44 +1,48 @@
 import pandas as pd
 from datetime import datetime
+import io
+from firebase_admin import storage
 
-# Caminho para o arquivo Excel
-file_path = r'C:\Users\tito\OneDrive\Documentos\MBV\Madeireira\VENDAS\Power Bi\Fat_Ctrl\Faturamento_total.xlsx'
-entrada_fiscal_path = r'C:\Users\tito\OneDrive\Documentos\MBV\Madeireira\Entrada\Entrada Fiscal.xlsx'
-
-def carregar_dados_excel():
-    """Carrega os dados do arquivo Excel."""
+def carregar_dados_excel(nome_arquivo):
+    """Carrega os dados do arquivo Excel do Firebase Storage."""
     try:
-        df = pd.read_excel(file_path)
+        bucket = storage.bucket()
+        blob = bucket.blob(f'SallesApp/{nome_arquivo}')
+        
+        # Download do arquivo para a memória
+        data = blob.download_as_string()
+        
+        # Criar um DataFrame a partir dos dados em memória
+        df = pd.read_excel(io.BytesIO(data))
+        
         df.columns = df.columns.str.strip().str.lower()
-        df['data'] = pd.to_datetime(df['data']).dt.date
+        if 'data' in df.columns:
+            df['data'] = pd.to_datetime(df['data']).dt.date
         return df
     except Exception as e:
         print(f"Erro ao carregar dados: {e}")
         return None
 
 def obter_faturamento_anual_todos():
-    df = carregar_dados_excel()
+    df = carregar_dados_excel('Faturamento_total.xlsx')
     if df is None:
         return []
-    # Aplicar o filtro para gru = "21" ou "21.0" ou "021"
     df_filtered = df[df['gru'].astype(str).isin(['21', '21.0', '021'])]
     faturamento_anual = df_filtered.groupby(df_filtered['data'].apply(lambda x: x.year))['valor total liquido'].sum()
     return [(int(ano), float(total)) for ano, total in faturamento_anual.items()]
 
 def obter_faturamento_mensal_todos():
-    df = carregar_dados_excel()
+    df = carregar_dados_excel('Faturamento_total.xlsx')
     if df is None:
         return []
-    # Aplicar o filtro para gru = "21" ou "21.0" ou "021"
     df_filtered = df[df['gru'].astype(str).isin(['21', '21.0', '021'])]
     faturamento_mensal = df_filtered.groupby([df_filtered['data'].apply(lambda x: x.year), df_filtered['data'].apply(lambda x: x.month)])['valor total liquido'].sum()
     return [(int(ano), int(mes), float(total)) for (ano, mes), total in faturamento_mensal.items()]
 
 def obter_faturamento_por_natureza(ano, mes):
-    df = carregar_dados_excel()
+    df = carregar_dados_excel('Faturamento_total.xlsx')
     if df is None:
         return []
-    # Aplicar o filtro para gru diferente de "21", "21.0" e "021"
     df_filtered = df[~df['gru'].astype(str).isin(['21', '21.0', '021'])]
     df_filtered = df_filtered[(df_filtered['data'].apply(lambda x: x.year) == ano) & (df_filtered['data'].apply(lambda x: x.month) == mes)]
     faturamento_por_natureza = df_filtered.groupby(df_filtered['nature'].str.strip().str.lower())['valor total liquido'].sum()
@@ -47,10 +51,11 @@ def obter_faturamento_por_natureza(ano, mes):
 def obter_entradas_mensais():
     """Carrega os dados de entrada de mercadorias por mês e ano."""
     try:
-        df = pd.read_excel(entrada_fiscal_path)
+        df = carregar_dados_excel('Entrada Fiscal.xlsx')
+        if df is None:
+            return []
+        
         print(f"Colunas no arquivo: {df.columns}")  # Depuração
-        df.columns = df.columns.str.strip().str.lower()
-        print(f"Colunas após processamento: {df.columns}")  # Depuração
         
         if 'dt_nota' not in df.columns:
             print("Coluna 'dt_nota' não encontrada!")  # Depuração
@@ -66,4 +71,3 @@ def obter_entradas_mensais():
     except Exception as e:
         print(f"Erro ao carregar dados de entrada: {e}")
         return []
-    
